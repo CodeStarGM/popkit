ccc1fba393e14ea23e3a12a2ced4a0eff397c882
CodeStarGM <codestargm@gmail.com>
2024-03-25 21:02:38 +0500

    heading to title

--- src/components/Popup/index.stories.tsx
+++ src/components/Popup/index.stories.tsx
@@ -11,7 +11,7 @@ const Template: StoryFn = (args: any) => <Popup {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  heading: "Blog post published",
+  title: "Blog post published",
   description:
     "This blog post has been published. Team members will be able to edit this post and republish changes.",
   open: false,
