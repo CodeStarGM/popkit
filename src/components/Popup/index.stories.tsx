4eb248f533002bca02f4464a002f5dea6b589301
CodeStarGM <codestargm@gmail.com>
2024-03-25 22:31:39 +0500

    add variants

--- src/components/Popup/index.stories.tsx
+++ src/components/Popup/index.stories.tsx
@@ -9,12 +9,24 @@ export default {
 
 const Template: StoryFn = (args: any) => <Popup {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
+export const Primary = Template.bind({});
+Primary.args = {
   title: "Blog post published",
   description:
-    "This blog post has been published. Team members will be able to edit this post and republish changes.",
-  open: false,
+    "This blog post has been published. Team members will be able to edit this post and republish changes, Team members will be able to edit this post and republish changes. ",
+  variant: "primary",
+  open: true,
+  setOpen: () => {},
+  onConfirm: () => {},
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  title: "Blog post published",
+  description:
+    "This blog post has been published. Team members will be able to edit this post and republish changes, Team members will be able to edit this post and republish changes. ",
+  variant: "danger",
+  open: true,
   setOpen: () => {},
   onConfirm: () => {},
 };
